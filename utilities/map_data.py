926f06d9c8483241474c35a5b8a468132cc1078a
jamesanderson-27 <james.anderson@dexcarehealth.com>
2025-10-02 09:46:44 -0600

    Update map_data.py

--- utilities/map_data.py
+++ utilities/map_data.py
@@ -1,12 +1,11 @@
 # Data mapper - these are called after the customer completes data mapping
 
-def saveFieldMapping(data_map,field,primary_data_source,primary_data_col,secondary_data_source,secondary_data_col,data_type,default_value):
+def saveFieldMapping(data_map,field,primary_data_source,primary_data_col,secondary_data_source,secondary_data_col,default_value):
     data_map["schema"][field]={
         "primary_data_source":primary_data_source,
         "primary_data_col":primary_data_col,
         "secondary_data_source":secondary_data_source,
         "secondary_data_col":secondary_data_col,
-        "data_type":data_type,
         "default_value":default_value
     }
     return data_map
