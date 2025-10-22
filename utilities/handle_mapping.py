73c31960a82d725e706d88b988610e9bb77f8495
jamesanderson-27 <james.anderson@dexcarehealth.com>
2025-10-22 16:13:48 -0600

    Update handle_mapping.py

--- utilities/handle_mapping.py
+++ utilities/handle_mapping.py
@@ -95,11 +95,10 @@ def sidebarMapping(view_customer,customer,data_map):
         st.badge("Draft Mapping",color="red")
         if not st.session_state.file_locked:
             st.markdown("*No draft in progress*")
-            pass
+        elif view_customer!=customer:
+            st.markdown(schemaToMarkdown(st.session_state[f"{view_customer}_current_data_map"]),unsafe_allow_html=True)
         elif view_customer==customer:
             st.markdown(schemaToMarkdown(data_map),unsafe_allow_html=True)
-        else:
-            st.markdown(schemaToMarkdown(st.session_state[f"{view_customer}_current_data_map"]),unsafe_allow_html=True)
     else: 
         st.badge("Current Mapping",color="grey")
         st.markdown(schemaToMarkdown(st.session_state[f"{view_customer}_current_data_map"]),unsafe_allow_html=True)
